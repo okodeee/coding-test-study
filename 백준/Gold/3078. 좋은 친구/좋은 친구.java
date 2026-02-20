ee7e0e819ccbee3bcd63644af5e903c1b7902dbc
okodeee <120557382+okodeee@users.noreply.github.com>
2026-02-20 11:54:53 +0900

    [Gold IV] Title: 좋은 친구, Time: 244 ms, Memory: 36448 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.java
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.java
@@ -9,31 +9,20 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] scoreOrder = new int[N];
+        int[] students = new int[N];
+        int[] counts = new int[21];
+        long answer = 0L;
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            scoreOrder[i] = str.length();
-        }
+            students[i] = br.readLine().length();
+            answer += counts[students[i]];
 
-        long answer = 0;
-        int[] length = new int[21]; // 윈도우 내에 해당 길이 이름인 학생이 몇 명인지 저장
-        for (int i = 0; i < K; i++) {
-            length[scoreOrder[i]]++;
-        }
-
-        for (int i = 0; i < N - K; i++) {
-            length[scoreOrder[i]]--;
-            length[scoreOrder[i + K]]++;
-
-            answer += length[scoreOrder[i]];
-        }
-
-        for (int i = 1; i < 21; i++) {  // 마지막 윈도우 내에서의 쌍 구하기
-            while (length[i] > 1) {
-                answer += length[i] - 1;
-                length[i]--;
+            // 범위 재구성
+            if (i - K >= 0) {
+                counts[students[i - K]]--;
             }
+            counts[students[i]]++;
         }
+
 
         System.out.println(answer);
     }
