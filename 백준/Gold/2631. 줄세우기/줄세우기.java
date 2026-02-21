31881e9d4006b5c7f50765d15e912ffcb1c7afc0
okodeee <120557382+okodeee@users.noreply.github.com>
2026-02-21 13:24:50 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -6,7 +6,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
         int[] children = new int[N];
         for (int i = 0; i < N; i++) {
             children[i] = Integer.parseInt(br.readLine());
@@ -14,22 +13,20 @@ public class Main {
 
         int[] dp = new int[N];
         dp[0] = 1;
+        int LIS = 0;
 
-        int result = 0;
-
-        for (int i = 1; i < N; i++) {   // i번쩨 아이까지의 LIS 구하기
+        for (int i = 1; i < N; i++) {
             dp[i] = 1;
 
             for (int j = i - 1; j >= 0; j--) {
-                if (children[i] > children[j]) {    // 업데이트
+                if (children[i] > children[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
 
-            result = Math.max(result, dp[i]);
-
+            LIS = Math.max(LIS, dp[i]);
         }
 
-        System.out.println(N - result);
+        System.out.println(N - LIS);
     }
 }
