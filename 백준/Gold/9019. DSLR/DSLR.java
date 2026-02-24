994533523c1bb16ae6e4638d80b200ab4208d8d4
okodeee <120557382+okodeee@users.noreply.github.com>
2026-02-24 14:47:20 +0900

    [Gold IV] Title: DSLR, Time: 2876 ms, Memory: 303236 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static class State {
+    static StringBuilder sb = new StringBuilder();
+
+    static class Value {
         int num;
         String command;
 
-        State(int num, String command) {
-            this.num = num;
-            this.command = command;
+        Value(int n, String c) {
+            num = n;
+            command = c;
         }
     }
 
@@ -18,63 +19,64 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
 
+        StringTokenizer st;
         while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
+            st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
 
-            System.out.println(bfs(A, B));
+            bfs(A, B);
         }
+
+        System.out.println(sb);
     }
 
-    static String bfs(int a, int b) {
+    static void bfs(int a, int b) {
         boolean[] visited = new boolean[10000];
-        Queue<State> q = new LinkedList<>();
+        Queue<Value> q = new LinkedList<>();
 
         visited[a] = true;
-        q.offer(new State(a, ""));
+        q.offer(new Value(a, ""));
 
-        while(!q.isEmpty()) {
-            State current = q.poll();
+        while (!q.isEmpty()) {
+            Value curr = q.poll();
 
-            if (current.num == b) {
-                return current.command;
+            if (curr.num == b) {
+                sb.append(curr.command).append('\n');
+                return;
             }
 
-            // D 연산
-            int next = (current.num * 2) % 10000;
+            // D
+            int next = (curr.num * 2) % 10000;
             if (!visited[next]) {
                 visited[next] = true;
-                q.offer(new State(next, current.command + "D"));
+                q.offer(new Value(next, curr.command + "D"));
             }
 
-            // S 연산
-            next = current.num - 1;
+            // S
+            next = curr.num - 1;
             if (next == -1) next = 9999;
             if (!visited[next]) {
                 visited[next] = true;
-                q.offer(new State(next, current.command + "S"));
+                q.offer(new Value(next, curr.command + "S"));
             }
 
-            // L 연산
-            next = current.num * 10;
+            // L
+            next = curr.num * 10;
             next += (next / 10000);
             next = next % 10000;
             if (!visited[next]) {
                 visited[next] = true;
-                q.offer(new State(next, current.command + "L"));
+                q.offer(new Value(next, curr.command + "L"));
             }
 
-            // R 연산
-            next = current.num + (current.num % 10) * 10000;
+            // R
+            next = curr.num + (curr.num % 10) * 10000;
             next /= 10;
             if (!visited[next]) {
                 visited[next] = true;
-                q.offer(new State(next, current.command + "R"));
+                q.offer(new Value(next, curr.command + "R"));
             }
         }
-
-        return "";
     }
 }
