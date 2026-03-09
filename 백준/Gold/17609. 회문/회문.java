b25fdf7261df27e0f327ffc3b177fa46e7c6336d
okodeee <120557382+okodeee@users.noreply.github.com>
2026-03-09 11:12:04 +0900

    [Gold V] Title: 회문, Time: 272 ms, Memory: 40960 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -2,47 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             String input = br.readLine();
-            sb.append(checkPalindrome(input)).append('\n');
+
+            sb.append(isPalindrome(input, 0, input.length() - 1, false)).append('\n');
         }
 
         System.out.println(sb);
     }
 
-    static int checkPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                // 왼쪽 제거 or 오른쪽 제거
-                if (isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)) {
-                    return 1;
-                }
-                return 2;
-            }
-            left++;
-            right--;
+    static int isPalindrome(String str, int s, int e, boolean skipped) {
+        if (s >= e) {
+            if (skipped) return 1;
+            return 0;
         }
 
-        return 0;
-    }
-
-    static boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
+        if (str.charAt(s) == str.charAt(e)) {
+            return isPalindrome(str, s + 1, e - 1, skipped);
+        } else if (str.charAt(s) != str.charAt(e) && !skipped) {
+            if (isPalindrome(str, s + 1, e, true) == 1 || isPalindrome(str, s, e - 1, true) == 1) {
+                return 1;
             }
-            left++;
-            right--;
         }
-        return true;
+
+        return 2;
     }
 }
