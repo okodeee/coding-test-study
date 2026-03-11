b5a9e95e5fef2863f82dc0de2f5ca6ac4709ef23
okodeee <120557382+okodeee@users.noreply.github.com>
2026-03-11 12:35:37 +0900

    [Gold III] Title: ABBC, Time: 280 ms, Memory: 27836 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.java
+++ 백준/Gold/25381. ABBC/ABBC.java
@@ -4,47 +4,48 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int n = s.length();
 
-        String str = br.readLine();
-        List<Character> temp = new LinkedList<>();
-
+        // B의 인덱스들을 저장할 큐 (A와 짝지을 용도, C와 짝지을 용도)
+        Deque<Integer> bIdxs = new ArrayDeque<>();
+        boolean[] used = new boolean[n];
         int result = 0;
 
-        int aCount = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'A') {
-                aCount++;
+        // 모든 B의 위치를 저장
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'B') bIdxs.add(i);
+        }
+
+        // B-C 매칭 (C 입장에서는 앞에 있는 B가 필요)
+        // 앞에서부터 순회하며 C를 만나면, 사용 가능한 가장 앞의 B를 매칭
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'C') {
+                // C보다 앞에 있는 B가 있는지 확인
+                while (!bIdxs.isEmpty() && bIdxs.peekFirst() < i) {
+                    int bPos = bIdxs.pollFirst();
+                    used[bPos] = true;
+                    used[i] = true;
+                    result++;
+                    break; // 매칭 성공 시 다음 C로
+                }
             }
         }
 
-        // 뒤에서부터 B와 A 조합 찾기
-        int bCount = 0;
-        for (int i = str.length() - 1; i >= 0; i--) {
-            char curr = str.charAt(i);
-            if (curr == 'B' && bCount < aCount) {
-                bCount++;
-            } else if (curr == 'B') {
-                temp.add(curr);
-            } else if (curr == 'A' && bCount > 0) {
-                result++;
-                aCount--;
-                bCount--;
-            } else if (curr == 'A') {
-                aCount--;
-            } else {
-                temp.add(curr);
-            }
-        }
-
-        // 뒤에서부터 C와 B 조합 찾기
-        int cCount = 0;
-        for (int i = 0; i < temp.size(); i++) {  // 역순으로 저장해둠
-            char curr = temp.get(i);
-            if (curr == 'C') {
-                cCount++;
-            } else if (curr == 'B' && cCount > 0) {
-                result++;
-                cCount--;
+        // A-B 매칭 (A 입장에서는 뒤에 있는 B가 필요)
+        // 뒤에서부터 순회하며 A를 만나면, 남은 B 중 가장 뒤에 있는 B를 매칭
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == 'A' && !used[i]) {
+                // A보다 뒤에 있는 B가 남아있는지 확인
+                while (!bIdxs.isEmpty() && bIdxs.peekLast() > i) {
+                    int bPos = bIdxs.pollLast();
+                    if (!used[bPos]) {
+                        used[bPos] = true;
+                        used[i] = true;
+                        result++;
+                        break;
+                    }
+                }
             }
         }
 
