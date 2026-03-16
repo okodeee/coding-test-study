5034a35171647713bf71b9e67cc31c1b6a873650
okodeee <120557382+okodeee@users.noreply.github.com>
2026-03-16 10:47:29 +0900

    [Gold V] Title: 동전, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -1,35 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
             int[] coins = new int[N];
-
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
                 coins[i] = Integer.parseInt(st.nextToken());
             }
 
             int M = Integer.parseInt(br.readLine());
-            int[] counts = new int[M + 1];    // 금액을 만드는 방법의 수
-            counts[0] = 1;  // 0원을 만드는 방법 1가지
+            int[] count = new int[M + 1];
+            count[0] = 1;
 
-            // 각 동전을 순서대로 사용
             for (int coin : coins) {
                 for (int i = coin; i <= M; i++) {
-                    counts[i] += counts[i - coin];
+                    count[i] += count[i - coin];
                 }
             }
 
-            sb.append(counts[M]).append('\n');
+            sb.append(count[M]).append('\n');
         }
 
         System.out.println(sb);
