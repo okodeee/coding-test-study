9d2f4f9853610d0303af1ea9574b3b044b147f33
okodeee <120557382+okodeee@users.noreply.github.com>
2026-03-21 14:21:27 +0900

    [Gold IV] Title: Two Dots, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.java
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -1,14 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static int[] dx = new int[] { 0, 1, -1, 0 };
-    static int[] dy = new int[] { 1, 0, 0, -1 };
     static int N, M;
     static char[][] board;
+    static int[] dx = new int[] { 0, 0, -1, 1 };
+    static int[] dy = new int[] { -1, 1, 0, 0 };
     static boolean[][] visited;
-    static boolean result = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,21 +15,20 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        // 보드 입력받기
         board = new char[N][M];
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < M; j++) {
-                board[i][j] = str.charAt(j);
+                board[i][j] = line.charAt(j);
             }
         }
 
         visited = new boolean[N][M];
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (!visited[i][j]) {
-                    cycle(i, j, i, j, board[i][j], 0, -1);
-                    if (result) {
+                    if (isCycle(i, j, -1, -1)) {
                         System.out.println("Yes");
                         return;
                     }
@@ -39,28 +36,29 @@ public class Main {
             }
         }
 
-//        cycle(0, 0, 0, 0, board[0][0], 0, -1);
-
         System.out.println("No");
     }
 
-    static void cycle(int startX, int startY, int x, int y, char state, int corner, int direction) {
+    static boolean isCycle(int x, int y, int px, int py) {
+        char color = board[x][y];
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
-            if (direction + i == 3) continue;   // 왔던 방향으로 다시 돌아가지 않도록
-
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] == state) {
-                if (visited[nx][ny]) {
-                    result = true;
-                    return;
-                }
-                cycle(startX, startY, nx, ny, state, i != direction ? corner + 1 : corner, i);
 
-                if (result) return;
-            }
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M || color != board[nx][ny]) continue;
+
+            // 부모 노드로 되돌아가는 경우는 무시
+            if (nx == px && ny == py) continue;
+
+            // 이미 방문한 노드를 다시 만남 = 사이클!
+            if (visited[nx][ny]) return true;
+
+            // 재귀 탐색
+            if (isCycle(nx, ny, x, y)) return true;
         }
+
+        return false;
     }
 }
