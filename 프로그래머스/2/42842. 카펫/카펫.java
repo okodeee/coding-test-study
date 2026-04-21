a90f31664098acdb03945e9d2e7c137cf4ed3f39
okodeee <120557382+okodeee@users.noreply.github.com>
2026-04-21 14:50:39 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int total = brown + yellow;
-
-        for (int h = 3; h <= total / h; h++) { // h는 최소 3부터 시작
+        
+        for (int h = 3; h <= total / h; h++) {
             if (total % h == 0) {
                 int w = total / h;
-                if ((w - 2) * (h - 2) == yellow) { // 노란색 개수 검증
-                    return new int[]{w, h};
+                
+                if ((w - 2) * (h - 2) == yellow) {
+                    return new int[] { w, h };
                 }
             }
         }
-        return null; // 실행되지 않음
+        
+        return null;
     }
 }
