61002ed9d2565cd4918a31778777df5bf87cf608
okodeee <120557382+okodeee@users.noreply.github.com>
2026-04-21 13:36:43 +0900

    [level 2] Title: 전화번호 목록, Time: 604.54 ms, Memory: 246 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 99.5 MB, 시간: 331.51 ms
+메모리: 246 MB, 시간: 604.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 08일 16:27:17
+2026년 04월 21일 13:36:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,15 +2,21 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book); // 전화번호를 정렬 (O(N log N))
-
-        // 정렬했으므로 인접한 번호만 비교하면 된다. (O(N))
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+        // 정렬하여 내가 이미 등록된 번호의 접두어인지 확인 못하는 경우 해결
+        Arrays.sort(phone_book);
+        
+        Map<String, Integer> subCnt = new HashMap<>();
+        
+        for (String phone : phone_book) {
+            for (int i = 1; i <= phone.length(); i++) {
+                if (subCnt.getOrDefault(phone.substring(0, i), 0) > 0) {
+                    return false;
+                }
             }
+            
+            subCnt.put(phone, 1);
         }
-
+        
         return true;
     }
 }
