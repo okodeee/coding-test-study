8406e29cf514317133e0bd6093635929ebb1e9f7
okodeee <120557382+okodeee@users.noreply.github.com>
2026-07-16 22:04:09 +0900

    [Passed] 부분 수열의 합이 M (121ms, 9MB)

---
+++ Codetree/trail4/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] seq = new int[N];
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int[] DP = new int[M+1];
+        Arrays.fill(DP, 100001);
+        DP[0] = 0;
+
+        for (int i = 0; i < N; i++) {
+            int num = seq[i];
+
+            for (int j = M; j >= num; j--) {
+                if (DP[j - num] != 100001) {
+                    DP[j] = Math.min(DP[j], DP[j - num] + 1);
+                }
+            }
+        }
+
+        System.out.println(DP[M] == 100001 ? -1 : DP[M]);
+    }
+}
