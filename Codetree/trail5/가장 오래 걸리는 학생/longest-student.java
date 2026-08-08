25823ecbc4283c4083bcd9463d33379a1467e741
okodeee <120557382+okodeee@users.noreply.github.com>
2026-08-08 20:38:57 +0900

    [Passed] 가장 오래 걸리는 학생 (1165ms, 48MB)

---
+++ Codetree/trail5/가장 오래 걸리는 학생/longest-student.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+class Edge {
+    int node;
+    int weight;
+
+    public Edge(int n, int w) {
+        this.node = n;
+        this.weight = w;
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Edge>[] graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            graph[u].add(new Edge(v, d));
+            graph[v].add(new Edge(u, d));
+        }   
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        int[] dis = new int[N+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[N] = 0;
+        pq.offer(new Edge(N, 0));
+
+        while (!pq.isEmpty()) {
+            Edge curr = pq.poll();
+
+            if (curr.weight > dis[curr.node]) continue;
+
+            for (Edge next : graph[curr.node]) {
+                if (dis[next.node] > dis[curr.node] + next.weight) {
+                    dis[next.node] = dis[curr.node] + next.weight;
+                    pq.offer(new Edge(next.node, dis[next.node]));
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i < N; i++) {
+            answer = Math.max(answer, dis[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
