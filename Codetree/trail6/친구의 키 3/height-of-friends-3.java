3c7012f082a81f3317e71cf50bd82f39f7778a76
okodeee <120557382+okodeee@users.noreply.github.com>
2026-09-08 17:51:14 +0900

    [Passed] 친구의 키 3 (453ms, 31MB)

---
+++ Codetree/trail6/친구의 키 3/height-of-friends-3.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static List<Integer>[] graph;
+    static int[] indegree;
+    static PriorityQueue<Integer> q = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        indegree = new int[N+1];
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            indegree[b]++;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) q.offer(i);
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int e = q.poll();
+            cnt++;
+            sb.append(e).append(" ");
+
+            for (int next : graph[e]) {
+                indegree[next]--;
+                if (indegree[next] == 0) q.offer(next);
+            }
+        }
+
+
+        if (cnt != N) {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println(sb);
+    }
+}
