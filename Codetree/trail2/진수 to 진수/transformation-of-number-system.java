04f9108cebb5f4cd36fd9934fd89c32ae979d452
okodeee <120557382+okodeee@users.noreply.github.com>
2026-09-14 23:43:59 +0900

    [Passed] 진수 to 진수 (128ms, 10MB)

---
+++ Codetree/trail2/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        String N = sc.next();
+        // Please write your code here.
+        System.out.println(Integer.toString(Integer.parseInt(N, A), B));
+    }
+}
