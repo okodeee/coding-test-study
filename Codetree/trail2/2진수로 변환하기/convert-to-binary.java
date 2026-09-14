420e1f862f9fdbd715f15a37989fd219588eff5f
okodeee <120557382+okodeee@users.noreply.github.com>
2026-09-14 23:32:05 +0900

    [Passed] 2진수로 변환하기 (135ms, 10MB)

---
+++ Codetree/trail2/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(Integer.toBinaryString(n));
+    }
+}
