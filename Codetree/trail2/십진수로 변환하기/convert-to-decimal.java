f355d251a0e399ee562259c745ccb860ed231cfa
okodeee <120557382+okodeee@users.noreply.github.com>
2026-09-14 23:35:28 +0900

    [Passed] 십진수로 변환하기 (122ms, 10MB)

---
+++ Codetree/trail2/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binary = sc.next();
+        // Please write your code here.
+
+        System.out.println(Integer.parseInt(binary, 2));
+    }
+}
